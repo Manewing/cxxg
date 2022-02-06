eda4d4e0841594092d1bccbe542636887229e881
Florian Walbroel <florian@hswalbroel.de>
2022-02-06 18:24:27 +0100

    tetris: fix: unintended early return

--- games/tetris/src/main.cpp
+++ games/tetris/src/main.cpp
@@ -4,15 +4,12 @@
 
 int main() {
   cxxg::Screen Scr(cxxg::Screen::getTerminalSize());
-
-  Tetris Game(Scr);
-  Game.initialize(false, 50000);
-  Game.run(false);
-  return 0;
-
   cxxg::utils::registerSigintHandler([]() { exit(0); });
 
+  Tetris Game(Scr);
   try {
+    Game.initialize(false, 50000);
+    Game.run(false);
   } catch (std::exception const &E) {
     std::cerr << "ERROR: " << E.what() << std::endl;
     return 1;
