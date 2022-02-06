9eb099e84033fde366f97a192358f62622a36654
Florian Walbroel <florian@hswalbroel.de>
2022-02-06 15:08:57 +0100

    libcxxg: add functionality for non-blocking getChar

--- lib/include/cxxg/Utils.h
+++ lib/include/cxxg/Utils.h
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <ctime>
 
 #define THROW_CXXG_ERROR(msg)                                                  \
   {                                                                            \
@@ -17,11 +18,26 @@ namespace cxxg {
 
 namespace utils {
 
+constexpr char KEY_INVALID = -1;
+constexpr char KEY_ESC = 27;
+constexpr char KEY_SPACE = 32;
+constexpr char KEY_UP = 65;
+constexpr char KEY_DOWN = 66;
+constexpr char KEY_RIGHT = 67;
+constexpr char KEY_LEFT = 68;
+
 /// Helper function for registering a handler (e.g. a lambda) for
 /// a SIGINT signal (after Ctrl+C). Note that only one handler can
 /// be registered.
 /// @param[in] Handler - Handler to register
 void registerSigintHandler(::std::function<void()> const &Handler);
+
+/// Helper function to check for keyboard input, returning single character.
+/// @param[in] Blocking If true waits until key is pressed
+int getChar(bool Blocking = true);
+
+/// Clears the current stdin buffer
+void clearStdin();
 
 /// Returns true if the terminal input is buffered or not. Buffered means
 /// that a '\n' is needed after input.
