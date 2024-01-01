ce704e9ff5f4d1c824aa5714c056f0fe55ad5fb5
Florian Walbroel <florian@hswalbroel.de>
2024-01-01 14:35:55 +0100

    rogue/scripts/rgb: allow to specify hex color

--- games/rogue/scripts/rgb.py
+++ games/rogue/scripts/rgb.py
@@ -2,12 +2,25 @@
 
 import sys
 
-if __name__ == "__main__":
-	r = int(sys.argv[1])
-	g = int(sys.argv[2])
-	b = int(sys.argv[3])
+def hex_to_rgb(hex: str):
+	h = hex.lstrip('#')
+	return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 
-	print(f"#{r:02x}{g:02x}{b:02x}")
+def rgb_to_hex(r: int, g: int, b: int):
+	return f"#{r:02x}{g:02x}{b:02x}"
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2 and len(sys.argv) != 4:
+		print("Usage: rgb.py <hex> or rgb.py <r> <g> <b>")
+		exit(1)
+	if len(sys.argv) == 2:
+		r, g, b = hex_to_rgb(sys.argv[1])
+	else:
+		r = int(sys.argv[1])
+		g = int(sys.argv[2])
+		b = int(sys.argv[3])
+
+	print(rgb_to_hex(r, g, b))
 	print("")
 	print(f"cxxg::types::RgbColor{{ {r}, {g}, {b} }}")
 	print("")
