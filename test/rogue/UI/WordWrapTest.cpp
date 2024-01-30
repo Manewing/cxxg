b5028bf51c1973123123447c1896afb7ba780bd2
Florian Walbroel <florian@hswalbroel.de>
2024-01-30 20:16:23 +0100

    rogue/UI/WordWrap: fix: make word wrap cut words that are too large to fit

--- test/rogue/UI/WordWrapTest.cpp
+++ test/rogue/UI/WordWrapTest.cpp
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
 #include <rogue/UI/WordWrap.h>
+#include <string>
+#include <vector>
 
 namespace {
+
+using LinesTy = std::vector<std::string>;
+LinesTy getLines(const rogue::ui::WordWrap &WW) {
+  LinesTy Lines;
+  for (std::size_t i = 0; i < WW.getNumLines(); ++i) {
+    EXPECT_LE(WW.getLine(i).size(), WW.getLineWidth());
+    Lines.push_back(std::string(WW.getLine(i)));
+  }
+  return Lines;
+}
 
 TEST(WordWrapTest, EmptyString) {
   rogue::ui::WordWrap WW("", 10);
@@ -13,75 +25,79 @@ TEST(WordWrapTest, EmptyString) {
 
 TEST(WordWrapTest, SingleWord) {
   rogue::ui::WordWrap WW("Hello", 10);
-  EXPECT_EQ(WW.getNumLines(), 1);
   EXPECT_EQ(WW.getLineWidth(), 10);
-  EXPECT_EQ(WW.getLine(0), "Hello");
+  EXPECT_EQ(getLines(WW), LinesTy{"Hello"});
 }
 
 TEST(WordWrapTest, SingleWordExactlyMatchingLineWidth) {
   std::string Text = "123456789";
   rogue::ui::WordWrap WW(Text, Text.size());
-  // FIXME this should be a single line
-  EXPECT_EQ(WW.getNumLines(), 2);
-  EXPECT_EQ(WW.getLine(0), "");
-  EXPECT_EQ(WW.getLine(1), "123456789");
+  LinesTy Ref = {Text};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, SingleReallyLongWord) {
   rogue::ui::WordWrap WW("Hellooooooooooooooooooooooooooooo", 10);
-  EXPECT_EQ(WW.getNumLines(), 2);
-  EXPECT_EQ(WW.getLine(0), "");
-  EXPECT_EQ(WW.getLine(1), "Hellooooooooooooooooooooooooooooo");
+  LinesTy Ref = {"Helloooooo", "oooooooooo", "oooooooooo", "ooo"};
+  EXPECT_EQ(getLines(WW), Ref);
+}
+
+TEST(WordWrapTest, SingleReallyLongWordWithTrailingSpace) {
+  rogue::ui::WordWrap WW("Hellooooooooooooooooooooooooooooo ", 10);
+  LinesTy Ref = {"Helloooooo", "oooooooooo", "oooooooooo", "ooo "};
+  EXPECT_EQ(getLines(WW), Ref);
+}
+
+TEST(WordWrapTest, SingleReallyLongWordWithLeadingSpace) {
+  rogue::ui::WordWrap WW(" Hellooooooooooooooooooooooooooooo", 10);
+  LinesTy Ref = {" Hellooooo", "oooooooooo", "oooooooooo", "oooo"};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, MultipleWords) {
   rogue::ui::WordWrap WW("Hello to this World", 10);
-  EXPECT_EQ(WW.getNumLines(), 3);
-  EXPECT_EQ(WW.getLine(0), "Hello to ");
-  EXPECT_EQ(WW.getLine(1), "this ");
-  EXPECT_EQ(WW.getLine(2), "World");
+  LinesTy Ref = {"Hello to ", "this World"};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, MultipleWordsOneReallyLong) {
-  rogue::ui::WordWrap WW("Hello toooooooooooooooooooooooooooooooo this World",
-                         10);
-  EXPECT_EQ(WW.getNumLines(), 4);
-  EXPECT_EQ(WW.getLine(0), "Hello ");
-  EXPECT_EQ(WW.getLine(1), "toooooooooooooooooooooooooooooooo ");
-  EXPECT_EQ(WW.getLine(2), "this ");
-  EXPECT_EQ(WW.getLine(3), "World");
+  rogue::ui::WordWrap WW(
+      "Hello tooooooooooooooooooooooooooooooooooo this World", 10);
+  LinesTy Ref = {"Hello tooo", "oooooooooo", "oooooooooo",
+                 "oooooooooo", "oo this ",   "World"};
+  EXPECT_EQ(getLines(WW), Ref);
+}
+
+TEST(WordWrapTest, MultipleWordsOneReallyLongNewLine) {
+  rogue::ui::WordWrap WW(
+      "Hello tooooooooooooooooooooooooooooooooooo\nthis World", 10);
+  LinesTy Ref = {"Hello tooo", "oooooooooo", "oooooooooo",
+                 "oooooooooo", "oo",         "this World"};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, MultipleWordsWithNewLine) {
   rogue::ui::WordWrap WW("Hello\nto this World", 10);
-  EXPECT_EQ(WW.getNumLines(), 3);
-  EXPECT_EQ(WW.getLine(0), "Hello ");
-  EXPECT_EQ(WW.getLine(1), "to this ");
-  EXPECT_EQ(WW.getLine(2), "World");
+  LinesTy Ref = {"Hello", "to this ", "World"};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, NewLineExactlyAtEndOfLine) {
   rogue::ui::WordWrap WW("123456789\n", 10);
-  EXPECT_EQ(WW.getNumLines(), 2);
-  EXPECT_EQ(WW.getLine(0), "123456789 ");
-  EXPECT_EQ(WW.getLine(1), " ");
+  LinesTy Ref = {"123456789", " "};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, NewLineAfterEndOfLine) {
   rogue::ui::WordWrap WW("0123456789\n", 10);
-  // FIXME we should get only two lines here
-  EXPECT_EQ(WW.getNumLines(), 3);
-  EXPECT_EQ(WW.getLine(0), "");
-  EXPECT_EQ(WW.getLine(1), "0123456789 ");
-  EXPECT_EQ(WW.getLine(2), " ");
+  LinesTy Ref = {"0123456789", " "};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 TEST(WordWrapTest, NewLineBeforeEndOfLine) {
   rogue::ui::WordWrap WW("\n0123456789", 10);
-  EXPECT_EQ(WW.getNumLines(), 3);
-  EXPECT_EQ(WW.getLine(0), " ");
-  EXPECT_EQ(WW.getLine(1), "");
-  EXPECT_EQ(WW.getLine(2), "0123456789");
+  LinesTy Ref = {"", "0123456789"};
+  EXPECT_EQ(getLines(WW), Ref);
 }
 
 } // namespace
